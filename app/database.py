451171fa0f847e95637c982a246b0713488e2889
Sparsh Mahajan <sparsh.1518@gmail.com>
2026-06-07 05:53:31 +0530

    setup FastAPI structure and databse config

---
+++ app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm.session import Session
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
